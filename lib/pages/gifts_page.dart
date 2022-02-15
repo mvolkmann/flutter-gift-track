0768aad73ea1e95b7b71f6726023173fdb7bcf34
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-02-15 14:35:39 -0600

    progress on Gifts page

--- lib/pages/gifts_page.dart
+++ lib/pages/gifts_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 import './gift_page.dart';
 import './my_page.dart';
+import '../app_state.dart';
 import '../widgets/my_text_button.dart';
 
 class GiftsPage extends StatelessWidget {
@@ -24,6 +26,57 @@ class GiftsPage extends StatelessWidget {
   }
 
   Widget _buildBody(BuildContext context) {
-    return Center(child: Text('List gifts here.'));
+    const itemHeight = 30.0;
+    const pickerHeight = 150.0;
+    final decoration = BoxDecoration(
+      border: Border.all(color: CupertinoColors.destructiveRed),
+    );
+
+    var appState = Provider.of<AppState>(context);
+    var occasions = appState.occasions.values.toList();
+    var people = appState.people.values.toList();
+    occasions.sort((o1, o2) => o1.name.compareTo(o2.name));
+    people.sort((p1, p2) => p1.name.compareTo(p2.name));
+
+    return Column(
+      children: [
+        Row(
+          children: [
+            Flexible(
+              child: Container(
+                child: CupertinoPicker.builder(
+                  childCount: people.length,
+                  itemBuilder: (_, index) => Text(people[index].name),
+                  itemExtent: itemHeight,
+                  onSelectedItemChanged: (index) {
+                    //selectedPerson = people[index],
+                    print('gifts_page.dart _buildBody: people index = $index');
+                  },
+                ),
+                decoration: decoration,
+                height: pickerHeight,
+              ),
+            ),
+            Flexible(
+              child: Container(
+                child: CupertinoPicker.builder(
+                  childCount: occasions.length,
+                  itemBuilder: (_, index) => Text(occasions[index].name),
+                  itemExtent: itemHeight,
+                  onSelectedItemChanged: (index) {
+                    //selectedOccasions = occasions[index],
+                    print(
+                        'gifts_page.dart _buildBody: occasions index = $index');
+                  },
+                ),
+                decoration: decoration,
+                height: pickerHeight,
+              ),
+            ),
+          ],
+        ),
+        Text('Add gift list here.'),
+      ],
+    );
   }
 }

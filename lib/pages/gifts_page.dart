5f5a126bd00ddfba4d21afeacc0567549dfa980a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-02-19 16:30:13 -0600

    more removing underscores

--- lib/pages/gifts_page.dart
+++ lib/pages/gifts_page.dart
@@ -25,10 +25,10 @@ class GiftsPage extends StatefulWidget {
 }
 
 class _GiftsPageState extends State<GiftsPage> {
-  late AppState _appState;
-  var _gifts = <Gift>[];
-  var _occasions = <Occasion>[];
-  var _people = <Person>[];
+  late AppState appState;
+  var gifts = <Gift>[];
+  var occasions = <Occasion>[];
+  var people = <Person>[];
 
   void _add(BuildContext context) {
     Navigator.push(
@@ -41,19 +41,19 @@ class _GiftsPageState extends State<GiftsPage> {
 
   @override
   Widget build(BuildContext context) {
-    _appState = Provider.of<AppState>(context);
+    appState = Provider.of<AppState>(context);
 
     return MyPage(
       title: 'Gifts',
-      child: _buildBody(context),
+      child: buildBody(context),
     );
   }
 
-  Widget _buildBody(BuildContext context) {
+  Widget buildBody(BuildContext context) {
     return Scaffold(
       floatingActionButton: MyFab(icon: CupertinoIcons.add, onPressed: _add),
       body: FutureBuilder(
-        future: _loadData(context),
+        future: loadData(context),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             return Text('Error fetching data: ${snapshot.error}');
@@ -66,7 +66,7 @@ class _GiftsPageState extends State<GiftsPage> {
             children: [
               GiftPickers(),
               SizedBox(height: 10),
-              for (var gift in _gifts) _buildListTile(gift),
+              for (var gift in gifts) _buildListTile(gift),
               Spacer(),
               Text(
                 'Total: ${formatPrice(_getTotal())}',
@@ -102,23 +102,23 @@ class _GiftsPageState extends State<GiftsPage> {
   }
 
   int _getTotal() =>
-      _gifts.fold(0, (int acc, Gift gift) => acc + (gift.price ?? 0));
+      gifts.fold(0, (int acc, Gift gift) => acc + (gift.price ?? 0));
 
-  Future<void> _loadData(BuildContext context) async {
-    if (_occasions.isEmpty) {
-      _occasions = _appState.sortedOccasions;
-      if (_occasions.isNotEmpty) {
-        await _appState.selectOccasion(_occasions[0], index: 0, silent: true);
+  Future<void> loadData(BuildContext context) async {
+    if (occasions.isEmpty) {
+      occasions = appState.sortedOccasions;
+      if (occasions.isNotEmpty) {
+        await appState.selectOccasion(occasions[0], index: 0, silent: true);
       }
     }
 
-    if (_people.isEmpty) {
-      _people = _appState.sortedPeople;
-      if (_people.isNotEmpty) {
-        await _appState.selectPerson(_people[0], index: 0, silent: true);
+    if (people.isEmpty) {
+      people = appState.sortedPeople;
+      if (people.isNotEmpty) {
+        await appState.selectPerson(people[0], index: 0, silent: true);
       }
     }
 
-    _gifts = _appState.gifts.values.toList();
+    gifts = appState.gifts.values.toList();
   }
 }
